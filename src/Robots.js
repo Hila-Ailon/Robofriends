a1db7285ede2df185a75d04c43d55766a9a6744f
Hila-Ailon <53815040+Hila-Ailon@users.noreply.github.com>
2019-11-22 16:03:31 +0200

    Project added

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots =[
+    {
+        id: 1,
+        name: 'Fer Fer',
+        username: 'Feri',
+        email: 'Ferfer@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Mickey Mouse',
+        username: 'mickey',
+        email: 'mickey@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Donald Duck',
+        username: 'donald',
+        email: 'donald@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Pluto',
+        username: 'pluto',
+        email: 'pluto@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Goofy',
+        username: 'goofy12',
+        email: 'goofy@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Minney Mouse',
+        username: 'minney',
+        email: 'minney@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Spider-Man',
+        username: 'Peter Parker',
+        email: 'spiderman@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Batman',
+        username: 'Bruce Wayne',
+        email: 'batman@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Hulk',
+        username: 'Robert Bruce Banner',
+        email: 'hulk@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Wonderwoman',
+        username: 'Diana Price',
+        email: 'wonderwoman@gmail.com'
+    }
+];
