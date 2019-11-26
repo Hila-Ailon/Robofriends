6c5bf51b69dd7645961219b5a2b9000d14a28861
Hila-Ailon <53815040+Hila-Ailon@users.noreply.github.com>
2019-11-26 09:49:10 +0200

    Updates

---
+++ precache-manifest.9293468f2b6a825ec2d534d7fdfa0c66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02224fed063523a5a66b6dc60cea461a",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "b57f5d967a1abb3e8ea3",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f842958424fe1101c271",
+    "url": "/Robofriends/static/css/main.57a60720.chunk.css"
+  },
+  {
+    "revision": "b57f5d967a1abb3e8ea3",
+    "url": "/Robofriends/static/js/2.40a8db71.chunk.js"
+  },
+  {
+    "revision": "f842958424fe1101c271",
+    "url": "/Robofriends/static/js/main.35c48708.chunk.js"
+  },
+  {
+    "revision": "a3eff8e1727534c00f37",
+    "url": "/Robofriends/static/js/runtime-main.11a3c7d6.js"
+  }
+]);
